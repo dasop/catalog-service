ffdc9f156647f713045098e177fa01c82055ab42
dasop <oragnedasop@naver.com>
2024-07-06 16:14:38 +0900

    config-repo

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,12 +1,20 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController{
+
+    private final PolarProperties PolarProperties;
+
+    public HomeController(PolarProperties PolarProperties){
+        this.PolarProperties = PolarProperties;
+    }
+
     @GetMapping("/")
     public String getGreeting(){
-        return "도서 카탈로그에 오신 것을 환영합니다!";
+        return PolarProperties.getGreeting();
     }
 }
